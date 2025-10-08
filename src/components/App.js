7f2533d8c3fbc3fdc184f4f5713514bb9d55611a
allanndunda <allanndunda>
2025-10-08 19:45:42 +0300

    not fully done

--- src/components/App.js
+++ src/components/App.js
@@ -3,7 +3,8 @@ import NavBar from "./NavBar";
 import Home from "./Home";
 import About from "./About";
 
-// pass this data down as props to the child component(s) that need it!
+// pass this data down as props
+//  to the child component(s) that need it!
 import user from "../data/user";
 console.log(user);
 
@@ -11,10 +12,15 @@ function App() {
   return (
     <div>
       <NavBar />
-      <Home />
-      <About />
+       <Home name={user.name} city={user.city} color={user.color} />
+      <About bio={user.bio} links={user.links} />
     </div>
   );
 }
 
 export default App;
+
+
+//  links: {
+//     github: "https://github.com/liza",
+//     linkedin: "https://www.linkedin.com/in/liza/",
